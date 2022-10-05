2d6145f45336be175a46047b8251f2fed7f0d968
JungHun98 <j7papa@naver.com>
2022-10-05 16:49:57 +0900

    factory pattern

--- Desing Pattern/FactoryPattern.java
+++ Desing Pattern/FactoryPattern.java
@@ -1,59 +1,90 @@
-class Pizza{
+class Pizza {
 
 }
 
-class CheesePizza extends Pizza{
+class CheesePizza extends Pizza {
 
 }
 
-class PepperoniPizza extends Pizza{
-    
+class PepperoniPizza extends Pizza {
+
 }
 
-class ClamPizza extends Pizza{
-    
+class ClamPizza extends Pizza {
+
 }
 
-class VeggiePizza extends Pizza{
-    
+class VeggiePizza extends Pizza {
+
 }
 
 class PizzaStore {
     SimplePizzaFactory factory;
 
-    public PizzaStore(SimplePizzaFactory factory){
+    public PizzaStore(SimplePizzaFactory factory) {
         this.factory = factory;
     }
 
-    public Pizza orderPizza(String type){
+    public Pizza orderPizza(String type) {
         Pizza pizza;
- 
-		pizza = factory.createPizza(type);
-        
+
+        pizza = factory.createPizza(type);
+
         return pizza;
     }
 }
-class SimplePizzaFactory {
-    
-    public Pizza createPizza(String type){
-        Pizza pizza = null;
-        
-        if (type.equals("cheese")) {
-			pizza = new CheesePizza();
-		} else if (type.equals("pepperoni")) {
-			pizza = new PepperoniPizza();
-		} else if (type.equals("clam")) {
-			pizza = new ClamPizza();
-		} else if (type.equals("veggie")) {
-			pizza = new VeggiePizza();
-		}
 
-		return pizza;
+class SimplePizzaFactory {
+
+    public Pizza createPizza(String type) {
+        Pizza pizza = null;
+
+        if (type.equals("cheese")) {
+            pizza = new CheesePizza();
+        } else if (type.equals("pepperoni")) {
+            pizza = new PepperoniPizza();
+        } else if (type.equals("clam")) {
+            pizza = new ClamPizza();
+        } else if (type.equals("veggie")) {
+            pizza = new VeggiePizza();
+        }
+
+        return pizza;
     }
 }
 
+abstract class PizzaStore2 {
+
+    public Pizza orderPizza(String type){
+        Pizza pizza;
+
+        pizza = createPizza(type);
+        // make pizza
+        return pizza;
+    }
+
+    protected abstract Pizza createPizza(String type);
+}
+
 public class FactoryPattern {
-    public static void main(String args[]){
+    // simplefactory를 method로 변환
+    public static Pizza orderPizza(String type) {
+        Pizza pizza = null;
+
+        if (type.equals("cheese")) {
+            pizza = new CheesePizza();
+        } else if (type.equals("pepperoni")) {
+            pizza = new PepperoniPizza();
+        } else if (type.equals("clam")) {
+            pizza = new ClamPizza();
+        } else if (type.equals("veggie")) {
+            pizza = new VeggiePizza();
+        }
+
+        return pizza;
+    }
+
+    public static void main(String args[]) {
 
     }
 }
